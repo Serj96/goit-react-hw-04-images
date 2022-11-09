c6eefc252485440abea9d16507df4e708597e067
Serj96 <99637869+Serj96@users.noreply.github.com>
2022-11-09 10:07:00 +0200

    modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,16 +2,31 @@ import { Component } from 'react';
 import css from './Modal.module.css';
 
 export default class Modal extends Component {
-  //   toggleModal = () => {
-  //     this.setState(({ showModal }) => ({
-  //       showModal: !showModal,
-  //     }));
-  //   };
+  static propTypes = {
+    toggleModal: PropTypes.func.isRequired,
+    largeImage: PropTypes.string.isRequired,
+  };
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handelKeyDown);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handelKeyDown);
+  }
+
+  handaleKeyDown = e => {
+    e.code === 'Escape' && this.props.toggleModal();
+  }
+
+  handelBackdropClick = e => {
+    e.target === e.currentTarget && this.props.toggleMpdal();
+  }
   render() {
+
     return (
-      <div className={css.overlay}>
+      <div className={css.overlay} onClick={this.handelBackdropClick}>
         <div className={css.modal}>
-          <img src="" alt="" />
+          <img src={this.props.images} alt="" />
         </div>
       </div>
     );
